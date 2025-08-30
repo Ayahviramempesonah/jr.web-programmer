cf0085b7987d18cf41286e0cb86737402e7cc71e
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2025-08-30 15:15:39 +0700

    fix database connection undefined

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,8 +1,20 @@
+"use server";
+
 import { neon } from "@neondatabase/serverless";
 
 export async function CreateContacts(formData: FormData) {
   // Connect to the Neon database
   const sql = neon(`${process.env.DATABASE_URL}`);
+  // if (!process.env.DATABASE_URL) {
+  //   console.error(
+  //     "Kesalahan Kritis: Environment variable DATABASE_URL tidak ditemukan.",
+  //   );
+  //   return {
+  //     error:
+  //       "Konfigurasi sisi server tidak valid. Gagal terhubung ke database.",
+  //   };
+  // }
+
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const message = formData.get("message") as string;
